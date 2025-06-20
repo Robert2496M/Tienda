5eaad40b48fca9ed9e29c5102f1a19e275566d7b
Roberto <Roberto@DESKTOP-PH0CQ69>
2025-06-19 21:38:49 -0600

    Semana 5

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,32 @@
+
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Categoria")
+public class Categoria implements Serializable {
+    
+    //Version de Serializacion
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Categoria")
+    private Long idCategoria; //lo trasnforma en id_Categoria
+    private String descripcion;
+    private String rutaImagen;
+    private boolean  activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, String rutaImagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    } 
+}
